a45e83045aea3fb0b58dbbe6cb635782de4d829a
Rob Capo <Robert.Capo@gtnetwork.com>
2015-02-25 09:09:59 -0500

    Updated rotating gaussians

--- src/RotatingGaussians.m
+++ src/RotatingGaussians.m
@@ -8,96 +8,42 @@ classdef RotatingGaussians < StreamingData
     %   Date: July, 2014
     
     properties (SetAccess = protected)
+        d = 2;                  % 2d dataset
         initialAngles = [];     % holds the initial angles of each class
         rotationFns = {};       % holds a cell array of functions that specify the rate at which each class rotates
         covariances = {};       % holds the covariances of each distribution
         rotationRates = [];     % holds the average rate (RPS) at which each distribution rotates
         radius = 5;             % holds the radius of the circle that the Gaussian's will rotate in
+        priors = [];
     end
     
     methods
-        function [x, y] = sample(obj, t, y)
-            if nargin < 3, y = obj.chooseClass(); end
-            if nargin < 2
-                if isempty(obj.t)
-                    t = 0;
-                else
-                    t = obj.t + obj.dt;
-                end
-            end
-            
-            obj.t = t;
-            
-            angle = obj.rotationFns{y}(obj.initialAngles(y), obj.t, 2*pi*obj.rotationRates(y));
-            mu = [obj.radius*cos(angle), obj.radius*sin(angle)];
-            x = mvnrnd(mu, obj.covariances{y}, 1);
-        end
-        
         function obj = RotatingGaussians(n)
             if nargin < 1, n = 2; end
+            obj.y = (1:n)';
+
+            obj.initialAngles = linspace(2*pi/n, 2*pi, n);
+
+            obj.covariances = cell(size(y));
+            obj.covariances(:) = {[1 0; 0 1]};
+            
+            obj.rotationRates = ones(size(obj.y));
+            obj.tMax = 2*max(obj.rotationRates);
+
+            obj.radius = 5;
+
+            obj.priors = ones(size(obj.y)) / n;
             
             obj.setNumberOfDistributions(n);
         end
-        
-        function setNumberOfDistributions(obj, n)
-            if nargin < 2, n = 2; end
+
+        function [x, y] = sample(obj, t, y)
+            if nargin < 3, y = obj.chooseClass(); end
+            if nargin < 2, t = 0; end
             
-            obj.n = n;
-            
-            obj.initialAngles = cumsum([0; (2*pi/n)*ones(obj.n-1, 1)]);
-            obj.classMix = 1/obj.n * ones(obj.n, 1);
-            obj.setCovariance();
-            obj.setRotationFn();
-            obj.setRotationRate();
-        end
-        
-        function setRadius(obj, r)
-            obj.radius = r;
-        end
-        
-        function setRotationRate(obj, i, r)
-            if nargin < 3, r = 1; end
-            if nargin < 2, i = 0; end
-            
-            if (obj.n > 0)
-                if (i == 0)
-                    obj.rotationRates = r*ones(obj.n, 1);
-                elseif (i <= obj.n)
-                    obj.rotationRates(i) = r;
-                end
-            end
-        end
-        
-        function setRotationFn(obj, i, fn)
-            if nargin < 3, fn = @RotatingGaussians.rotateLin; end
-            if nargin < 2, i = 0; end
-            
-            if (obj.n > 0)
-                if (size(obj.rotationFns, 1) ~= obj.n)
-                    obj.rotationFns = cell(obj.n, 1);
-                end
-                if (i == 0)
-                    [obj.rotationFns{:}] = deal(fn);
-                elseif (i <= obj.n)
-                    obj.rotationFns{i} = fn;
-                end
-            end
-        end
-        
-        function setCovariance(obj, i, cov)
-            if nargin < 3, cov = diag(ones(2,1)); end
-            if nargin < 2, i = 0; end
-            
-            if (obj.n > 0)
-                if (size(obj.covariances, 1) ~= obj.n)
-                    obj.covariances = cell(obj.n, 1);
-                end
-                if (i == 0)
-                    [obj.covariances{:}] = deal(cov);
-                elseif (i <= obj.n)
-                    obj.covariances{i} = cov;
-                end
-            end
+            angle = obj.rotateLin(obj.initialAngles(y), rotationRates(y), t);
+            mu = [obj.radius*cos(angle), obj.radius*sin(angle)];
+            x = mvnrnd(mu, obj.covariances{y}, 1);
         end
     end
     
