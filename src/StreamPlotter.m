edde1faf583bb0a4f5668cd0a59e70d78f865af3
Rob <robcapo@gmail.com>
2015-02-26 20:20:24 -0500

    Renamed plotter interface

---
+++ src/StreamPlotter.m
@@ -0,0 +1,25 @@
+classdef StreamPlotter < handle
+    %PLOTTER The plotter interface is used to plot a given data, label, and
+    %time value.
+    
+    properties (SetAccess = protected)
+        axh;
+        n = -1; % number of most recent points to retain on plot
+        colors  = [ 1 0 0;      % Red
+                    0 1 0;      % Green
+                    0 0 1;      % Blue
+                    0 1 1;      % Cyan
+                    1 0 1;      % Magenta
+                    1 1 0;      % Yellow
+                    0 0 0 ];    % Black
+    end
+    
+    methods (Abstract = true)
+        % Plots a new point or points
+        % X - vector or matrix of points to plot
+        % c - scalar or vector of color indices to plot each point in X in
+        plot(obj, X, c)
+    end
+    
+end
+
