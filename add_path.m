83e3a4487e7860ac4d3accbb677a822b5ff3e0bc
Rob <robcapo@gmail.com>
2015-03-08 15:34:53 -0400

    Added utility for adding this directory to the MATLAB path

---
+++ add_path.m
@@ -0,0 +1,33 @@
+function add_path()
+    thePath = genpath(pwd);
+    dirs = strsplit(thePath, ':');
+    i = 1;
+
+    while i <= length(dirs)
+        git = 0;
+
+        dir = dirs{i};
+        subDirs = strsplit(dir, '/');
+        for j = 1:length(subDirs)
+            subDir = subDirs{j};
+            if strcmp(subDir, '.git')
+                git = 1;
+            end
+        end
+
+        if git == 1
+            dirs(i) = [];
+        else
+            i = i + 1;
+        end
+    end
+
+    disp('Adding the following directories to the path: ');
+    for i = 1:length(dirs)
+        disp(dirs{i});
+    end
+    
+    thePath = [sprintf('%s:', dirs{1:end-1}), dirs{end}];
+    
+    addpath(thePath);
+end
