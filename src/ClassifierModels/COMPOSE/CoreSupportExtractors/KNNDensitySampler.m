9a9e74fa3d0d5d9ce88684953a392d8e35cac26c
Rob <robcapo@gmail.com>
2015-04-01 18:58:56 -0400

    Added docs

--- src/ClassifierModels/COMPOSE/CoreSupportExtractors/KNNDensitySampler.m
+++ src/ClassifierModels/COMPOSE/CoreSupportExtractors/KNNDensitySampler.m
@@ -14,12 +14,14 @@ classdef KNNDensitySampler < CoreSupportExtractor
         end
         
         function inds = extract(obj, data)
+            % returns NxK matrix of nearest neighbor distances
             [~, D] = knnsearch(data, data, 'K', obj.k);
             
+            % average over all neighbors
             D = mean(D, 2);
             
+            % return the observations with the lowest overall distance (highest density)
             [~, ind] = sort(D);
-            
             inds = ind(1:min(length(ind), round(obj.p * length(ind))));
         end
     end
