5b2296d004a06264a2b9e43c9ba0eb3fd2236ce1
Rob Capo <Robert.Capo@gtnetwork.com>
2015-03-31 20:49:05 -0400

    wip

--- src/ClassifierModels/COMPOSE/COMPOSE.m
+++ src/ClassifierModels/COMPOSE/COMPOSE.m
@@ -6,8 +6,11 @@ classdef COMPOSE < ClassifierModel
         classifier;
         core_support_extractor;
         
-        extractEvery = 1; % extract every time classify is called
+        autoExtract = 1; % extract every time classify is called 0
         classifyCount = 0; % number of times classify has been called since last extraction
+        
+        X = [];
+        y = [];
     end
     
     methods
@@ -24,6 +27,37 @@ classdef COMPOSE < ClassifierModel
             obj.ssl = ssl;
             
             if isfield(opts, 'extractEvery'), obj.extractEvery = opts.extractEvery; end
+        end
+        
+        function train(obj, X, y, ~)
+            obj.X = [obj.X; X];
+            obj.y = [obj.y; y];
+        end
+        
+        function h = classify(obj, X, ~)
+            h = obj.classifier.classify(obj.X, obj.y, X);
+            obj.classifyCount = obj.classifyCount + 1;
+            
+            if obj.classifyCount == obj.autoExtract
+                obj.y = obj.extract();
+            end
+        end
+        
+        function extract(obj)
+            obj.classifyCount = 0;
+            
+            classes = unique(obj.y);
+            
+            for i = 1:length(classes)
+                inds = obj.y == classes(i);
+                x = obj.X(inds, :);
+                
+                cs = obj.core_support_extractor.extract(x);
+                
+                inds(cs) = [];
+                obj.x(inds, :) = [];
+                obj.y(inds) = [];
+            end
         end
     end
     
