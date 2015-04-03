960a4a49871ee772ab9f9757ceed4125c4cded41
Rob <robcapo@gmail.com>
2015-04-03 00:43:05 -0400

    Can now specify number of cores to use. Also, put ashape last.

--- tests/cse/parallel_cse_comparison.m
+++ tests/cse/parallel_cse_comparison.m
@@ -1,14 +1,17 @@
-function results = parallel_cse_comparison(dataFun, n)
+function results = parallel_cse_comparison(dataFun, n, cores)
+if nargin < 3, cores = feature('numCores'); end
+if nargin < 2, n = 10; end
+
 results = cell(n, 2);
 cses = { ...
-    AlphaShapeCompactor(struct('p', .4)), ...
     KNNDensitySampler(struct('p', .4)), ...
     ParzenWindowSampler(struct('p', .4)), ...
     GMMDensitySampler(struct('p', .4)) ...
+    AlphaShapeCompactor(struct('p', .4)), ...
 };
 
 delete(gcp('nocreate'));
-pool = parpool(feature('numCores'));
+pool = parpool(cores);
 try
     parfor i = 1:n
         dataset = dataFun();
