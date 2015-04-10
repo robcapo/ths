d451a0829a34b3061b6b0d2422f1ee4fea0bfe1d
Rob <robcapo@gmail.com>
2015-04-09 21:51:24 -0400

    Added parallel datasets CSE comparison

---
+++ tests/cse/parallel_cse_comparison_dataset.m
@@ -0,0 +1,33 @@
+function results = parallel_cse_comparison_dataset(datasets, cores)
+if nargin < 2, cores = feature('numCores'); end
+
+n = length(datasets);
+
+results = cell(n, 2);
+cses = { ...
+    KNNDensitySampler(struct('p', .4)), ...
+    ParzenWindowSampler(struct('p', .4)), ...
+    GMMDensitySampler(struct('p', .4)) ...
+    AlphaShapeCompactor(struct('p', .4)), ...
+};
+
+delete(gcp('nocreate'));
+pool = parpool(cores);
+try
+    parfor i = 1:n
+        dataset = datasets{i};
+        results(i, :) = {dataset, compose_cse_comparison(dataset, cses)};
+    end
+    
+    save('results', 'results');
+    
+catch e
+    disp('Caught error, saving results');
+    save('results', 'results');
+    delete(pool);
+    warning('on', 'stats:gmdistribution:FailedToConverge');
+    rethrow(e);
+end
+
+save('parallel_cse_comparison_results_datasets', 'results');
+end
