ea5a2807ea6e5a584c91363757dd07e02202f34b
Rob Capo <Robert.Capo@gtnetwork.com>
2015-04-21 22:41:07 -0400

    Added confidence based threshold for training, organized functions

--- src/Classifiers/ForgettingKnnClassifier.m
+++ src/Classifiers/ForgettingKnnClassifier.m
@@ -13,8 +13,19 @@ classdef ForgettingKnnClassifier < ClassifierModel
         priors = [];
         
         d; % dimensions of data
+        
+        trainThreshold = .8;
+        
+        lastKnnX = [];
+        lastKnnY = [];
+        lastKnnT = [];
+        lastKnnD = [];
+        lastX = [];
+        lastY = [];
+        lastSampleUsedForTraining = 0;
     end
     
+    % Instantiation
     methods
         % Constructor
         %
@@ -27,7 +38,7 @@ classdef ForgettingKnnClassifier < ClassifierModel
             
             defaultOpts = struct;
             defaultOpts.rowPadding = 500;
-            defaultOpts.beta = .1;
+            defaultOpts.beta = 1;
             defaultOpts.k = 25;
             
             fields = fieldnames(defaultOpts);
@@ -39,53 +50,26 @@ classdef ForgettingKnnClassifier < ClassifierModel
             
             obj.opts = opts;
         end
+    end
         
-        
+    % ClassifierModel API
+    methods 
         % Train classifier with n observations
         %
         % X: nxd matrix of observations
         % y: nx1 or nxc binary matrix of class labels
         % t: nx1 times sampled
         function train(obj, X, y, t)
-            if isempty(obj.d), obj.d = size(X, 2); end
+            [y, Y] = obj.processYData(y);
             
-            if size(X, 1) ~= size(y, 1) || size(X, 1) ~= size(t, 1)
-                error('ths:Classifier:BadData', 'X, y, and t must all have the same number of rows');
-            end
+            trainInds = max(Y, [], 2) > obj.trainThreshold;
             
-            if size(X, 2) ~= obj.d
-                error('ths:Classifier:BadData', ['Dimensionality of X must be ', num2str(obj.d)]);
-            end
-            
-            if size(y, 2) == 1
-                Y = full(ind2vec(y'))';
+            if sum(trainInds) >= 1
+                obj.lastSampleUsedForTraining = 1;
+                obj.addData(X(trainInds, :), y(trainInds), Y(trainInds, :), t(trainInds));
             else
-                Y = y;
-                [~, y] = max(Y, [], 2);
+                obj.lastSampleUsedForTraining = 0;
             end
-            
-            n = size(X, 1);
-            
-            % Allocate more rows if our matrix is almost full
-            obj.growStorage(n);
-            
-            % Ensure that posterior matrix matches the current set of known classes
-            if size(Y, 2) > size(obj.Y, 2)
-                obj.Y = [obj.Y, zeros(size(obj.Y, 1), size(Y, 2) -  size(obj.Y, 2))];
-                obj.priors = [obj.priors, zeros(1, size(Y, 2) - size(obj.priors, 2))];
-            elseif size(Y, 2) < size(obj.Y, 2)
-                Y = [Y, zeros(size(Y, 1), size(obj.Y, 2) - size(Y, 2))];
-            end
-            
-            % Add new data to known data
-            ind = obj.maxInd + 1;
-            obj.X(ind:ind+n-1, :) = X;
-            obj.y(ind:ind+n-1, :) = y;
-            obj.Y(ind:ind+n-1, :) = Y;
-            obj.t(ind:ind+n-1, :) = t;
-            obj.priors = obj.priors + sum(Y, 1);
-            
-            obj.maxInd = obj.maxInd + size(X, 1);
         end
         
         
@@ -98,35 +82,94 @@ classdef ForgettingKnnClassifier < ClassifierModel
             
             H = zeros(size(X, 1), size(obj.Y, 2));
             for i = 1:size(X, 1)
-                H(i, :) = obj.learn(X(i, :), t(i));
+                H(i, :) = obj.classifyInstance(X(i, :), t(i));
             end
             
             obj.train(X, H, t);
             [~, h] = max(H, [], 2);
         end
-        
-        % Gets current data (ignoring empty allocated rows)
-        function [X, y, Y, t] = getData(obj)
-            X = obj.X(1:obj.maxInd, :);
-            y = obj.y(1:obj.maxInd, :);
-            Y = obj.Y(1:obj.maxInd, :);
-            t = obj.t(1:obj.maxInd, :);
-        end
-        
     end
     
+    % Public API for ForgettingKnnClassifier
+    methods
+        % Gets current data (ignoring empty allocated rows)
+        function [X, y, Y, t] = getData(obj, minInd, maxInd)
+            if nargin < 3, maxInd = obj.maxInd; end
+            if nargin < 2, minInd = 1; end
+            
+            X = obj.X(minInd:maxInd, :);
+            y = obj.y(minInd:maxInd, :);
+            Y = obj.Y(minInd:maxInd, :);
+            t = obj.t(minInd:maxInd, :);
+        end
+    end
+        
+    
     methods (Hidden, Access = protected)
-        % Learn a signle observation
+        function [y, Y] = processYData(~, y)
+            if size(y, 2) == 1
+                Y = full(ind2vec(y'))';
+            else
+                Y = y;
+                [~, y] = max(Y, [], 2);
+            end
+        end
+        
+        function addData(obj, X, y, Y, t)
+            if isempty(obj.d), obj.d = size(X, 2); end
+            
+            if size(X, 1) ~= size(y, 1) || size(X, 1) ~= size(t, 1) || size(X, 1) ~= size(Y, 1)
+                error('ths:Classifier:BadData', 'X, y, and t must all have the same number of rows');
+            end
+            
+            if size(X, 2) ~= obj.d
+                error('ths:Classifier:BadData', ['Dimensionality of X must be ', num2str(obj.d)]);
+            end
+            
+            % Ensure that posterior matrix matches the current set of known classes
+            if size(Y, 2) > size(obj.Y, 2)
+                obj.Y = [obj.Y, zeros(size(obj.Y, 1), size(Y, 2) -  size(obj.Y, 2))];
+                obj.priors = [obj.priors, zeros(1, size(Y, 2) - size(obj.priors, 2))];
+            elseif size(Y, 2) < size(obj.Y, 2)
+                Y = [Y, zeros(size(Y, 1), size(obj.Y, 2) - size(Y, 2))];
+            end
+            
+            obj.priors = obj.priors + sum(Y, 1);
+            
+            n = size(X, 1);
+            
+            % Allocate more rows if our matrix is almost full
+            obj.growStorage(n);
+            
+            % Append data to dynamic matrix
+            ind = obj.maxInd + 1;
+            obj.X(ind:ind+n-1, :) = X;
+            obj.y(ind:ind+n-1, :) = y;
+            obj.Y(ind:ind+n-1, :) = Y;
+            obj.t(ind:ind+n-1, :) = t;
+            
+            obj.maxInd = obj.maxInd + size(X, 1);
+        end
+        
+        % Classify a single observation
         %
         % h: 1xc posterior vector
         %
         % x: 1xd observation vector
         % t: 1x1 time drawn
-        function h = learn(obj, x, t)
-            [dk, ~, Yk, tk] = obj.findKnnWithForgetting(x, t);
+        function h = classifyInstance(obj, x, t)
+            [dk, Xk, Yk, tk] = obj.findKnnWithForgetting(x, t);
             
             h = sum(repmat(exp(-dk), 1, size(Yk, 2)) .* Yk, 1);
             h = h / sum(h);
+            
+            
+            obj.lastKnnX = Xk;
+            obj.lastKnnY = Yk;
+            obj.lastKnnT = tk;
+            obj.lastKnnD = dk;
+            obj.lastX = x;
+            obj.lastY = h;
         end
         
         % Find k nearest neighbors
@@ -159,7 +202,7 @@ classdef ForgettingKnnClassifier < ClassifierModel
             
             classPriors = obj.priors / sum(obj.priors, 2);
             
-            dt = obj.opts.beta*classPriors(y)'.*abs(t - ttr);
+            dt = exp(abs(t - ttr).^obj.opts.beta);
             
             % Find order of nearest neighbors
             d = pdist2([Xtr, dt], [x, zeros(size(x, 1), 1)]);
