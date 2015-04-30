a6283351ce27dc573c5a0c1ec3cb50f982d67a63
Rob <robcapo@gmail.com>
2015-04-29 22:30:28 -0400

    Added simple param sweep for ForgettingKnn

---
+++ scripts/run_knn_param_sweep.m
@@ -0,0 +1,15 @@
+bestK = 5;
+bestBeta = .2102;
+
+best = 0;
+for beta = linspace(0.1, 1, 50)
+    for k = 1:2:40
+        a = run_online_dataset(ForgettingKnnClassifier(struct('beta', beta, 'k', k)), X, y, t, 300);
+        perf = sum(a.y == a.h) / length(a.y);
+        if perf > best
+            best = perf;
+            bestK = k;
+            bestBeta = beta;
+        end
+    end
+end
